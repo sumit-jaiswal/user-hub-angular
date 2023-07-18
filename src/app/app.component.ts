e6066cd022c97aa53eebf29825d0e089b22d328d
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-07-18 09:10:36 +0100

    clear root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { SocialAuthService } from '@abacritt/angularx-social-login';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'user-hub-angular';
+export class AppComponent implements OnInit {
+  constructor(private socialLoginService: SocialAuthService) {}
+
+  ngOnInit() {}
 }
