64d4a26c8bca66581e6889a5f56155e5e5ae257f
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-07-24 07:51:09 +0100

    Login component unit test case

--- src/app/core/auth/login/login.component.spec.ts
+++ src/app/core/auth/login/login.component.spec.ts
@@ -1,77 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { LoginComponent } from './login.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthService } from '../auth.service';
-import { SocialAuthServiceConfigMock } from 'src/app/testing/social-auth.mock';
-import { MaterialModule } from 'src/app/shared/material.module';
 import {
   GoogleSigninButtonModule,
-  SocialAuthService,
   SocialLoginModule,
 } from '@abacritt/angularx-social-login';
-import { Router } from '@angular/router';
+import { MaterialModule } from 'src/app/shared/material.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SocialAuthServiceConfigMock } from 'src/app/testing/social-auth.mock';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let socialAuthService: jasmine.SpyObj<SocialAuthService>;
-  let authService: jasmine.SpyObj<AuthService>;
-  let router: jasmine.SpyObj<Router>;
+  let authService: AuthService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginComponent],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [
+        MaterialModule,
         SocialLoginModule,
         GoogleSigninButtonModule,
+        RouterTestingModule,
         HttpClientTestingModule,
-        MaterialModule,
       ],
-      providers: [
-        HttpClientTestingModule,
-        AuthService,
-        SocialAuthServiceConfigMock,
-      ],
+      declarations: [LoginComponent],
+      providers: [AuthService, SocialAuthServiceConfigMock],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-
-    socialAuthService = TestBed.inject(
-      SocialAuthService
-    ) as jasmine.SpyObj<SocialAuthService>;
-    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
-    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
-    fixture.detectChanges();
+    authService = TestBed.inject(AuthService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should navigate to /users when user is logged in via social authentication', () => {
-    const routingSpy = spyOn(router, 'navigate');
-    const user = { idToken: 'some-id-token' };
-    authService.login(user.idToken);
-    component.checkIsLogin();
-    expect(routingSpy).toHaveBeenCalledWith(['/users']);
+  it('should navigate to "/users" when onLoginSuccess is called', () => {
+    let navigateSpy = spyOn(component['router'], 'navigate');
+    component.onLoginSuccess();
+    expect(navigateSpy).toHaveBeenCalledWith(['/users']);
   });
 
-  it('should navigate to /users when user is logged in via regular authentication', () => {
-    const routingSpy = spyOn(router, 'navigate');
-    const user = { idToken: 'some-id-token' };
-    authService.login(user.idToken);
-
-    component.checkIsLogin();
-    expect(routingSpy).toHaveBeenCalledWith(['/users']);
-  });
-
-  it('should navigate to /users when user is already authenticated', () => {
-    const routingSpy = spyOn(router, 'navigate');
+  it('should call onLoginSuccess when authService.isAuthenticated() returns true', () => {
+    let navigateSpy = spyOn(component['router'], 'navigate');
     spyOn(authService, 'isAuthenticated').and.returnValue(true);
+    component.ngOnInit();
+    expect(navigateSpy).toHaveBeenCalledWith(['/users']);
+  });
 
-    component.checkIsLogin();
-    expect(routingSpy).toHaveBeenCalledWith(['/users']);
+  it('should call onLoginSuccess when authService.isAuthenticated$ returns true', () => {
+    let navigateSpy = spyOn(component['router'], 'navigate');
+    spyOn(authService, 'isAuthenticated').and.returnValue(true);
+    authService['checkAuthenticationStatus']();
+    component.ngOnInit();
+    expect(navigateSpy).toHaveBeenCalledWith(['/users']);
   });
 });
