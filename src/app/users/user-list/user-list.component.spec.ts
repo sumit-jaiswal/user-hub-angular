f38d254fc366d53012855d0fc3e087833f09e421
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-07-24 07:53:28 +0100

    Add filter in users list

--- src/app/users/user-list/user-list.component.spec.ts
+++ src/app/users/user-list/user-list.component.spec.ts
@@ -6,10 +6,16 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MaterialModule } from 'src/app/shared/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { UsersServiceMock } from 'src/app/testing/users-service.mock';
+import { LoadingService } from 'src/app/shared/services/loading.service';
+import { Subject, of } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let usersService: UsersService;
+  let loadingService: LoadingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,15 +26,203 @@ describe('UserListComponent', () => {
         BrowserAnimationsModule,
         SharedModule,
       ],
-      providers: [UsersService],
+      providers: [{ provide: UsersService, useClass: UsersServiceMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+    usersService = TestBed.inject(UsersService);
+    loadingService = TestBed.inject(LoadingService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should initialize pagination settings', () => {
+      component.initializePagination();
+      expect(component.pageSize).toBe(10);
+      expect(component.pageIndex).toBe(0);
+      expect(component.totalUsers).toBe(0);
+    });
+
+    it('should call getUsers', () => {
+      spyOn(component, 'getUsers').and.callThrough();
+      spyOn(loadingService, 'loadingOn');
+
+      component.getUsers();
+      expect(component.getUsers).toHaveBeenCalled();
+      expect(loadingService.loadingOn).toHaveBeenCalled();
+    });
+  });
+
+  describe('getPaginationData', () => {
+    it('should call getUsers from UsersService and set users in MatTableDataSource', (done) => {
+      spyOn(usersService, 'getUsers').and.callThrough();
+      spyOn(component, 'applySortingOnResponse');
+      spyOn(loadingService, 'loadingOff');
+
+      const filterValue = 'John';
+      component.getPaginationData(filterValue).subscribe((users) => {
+        expect(usersService.getUsers).toHaveBeenCalledWith(
+          component.pageIndex,
+          component.pageSize,
+          filterValue
+        );
+        expect(component.applySortingOnResponse).toHaveBeenCalledWith(users);
+        done();
+      });
+    });
+  });
+
+  describe('onPageChange', () => {
+    it('should update pageIndex and pageSize properties', () => {
+      const event: PageEvent = { pageIndex: 2, pageSize: 10, length: 50 };
+      component.onPageChange(event);
+
+      expect(component.pageIndex).toBe(2);
+      expect(component.pageSize).toBe(10);
+    });
+
+    it('should call loadingOn method of loadingService', () => {
+      spyOn(loadingService, 'loadingOn');
+      const event: PageEvent = { pageIndex: 0, pageSize: 5, length: 20 };
+      component.onPageChange(event);
+
+      expect(loadingService.loadingOn).toHaveBeenCalled();
+    });
+  });
+
+  describe('filterUsers', () => {
+    beforeEach(() => {
+      spyOn(loadingService, 'loadingOn');
+      spyOn(component, 'getPaginationData').and.returnValue(of([]));
+    });
+    it('should do nothing when event or event.target is not provided', () => {
+      const event: any = null;
+      component.filterUsers(event);
+      expect(component.getPaginationData).not.toHaveBeenCalled();
+      expect(loadingService.loadingOn).not.toHaveBeenCalled();
+    });
+    it('should cancel previous API call and call loadingOn when a non-empty filter value is provided', () => {
+      const inputValue = 'example filter';
+      const event: any = { target: { value: inputValue } };
+
+      component.filterUsers(event);
+
+      expect(component.getPaginationData).toHaveBeenCalledWith(
+        inputValue.toLowerCase()
+      );
+      expect(loadingService.loadingOn).toHaveBeenCalled();
+    });
+
+    it('should debounce the API call by 300ms', (done: DoneFn) => {
+      const debounceTimeValue = 300;
+
+      const event: any = { target: { value: 'sample filter' } };
+      component.filterUsers(event);
+
+      expect(component.getPaginationData).toHaveBeenCalledWith('sample filter');
+
+      // Wait for the debounce time plus a little buffer (50ms) before checking the getPaginationData call count.
+      setTimeout(() => {
+        expect(component.getPaginationData).toHaveBeenCalledTimes(1);
+        done();
+      }, debounceTimeValue + 50);
+    });
+    it('should call clearFilterUsers when an empty filter value is provided', () => {
+      const event: any = { target: { value: '' } };
+      spyOn(component, 'clearFilterUsers');
+
+      component.filterUsers(event);
+
+      expect(component.clearFilterUsers).toHaveBeenCalled();
+    });
+  });
+  describe('clearFilterUsers', () => {
+    it('should call loadingOn method of loadingService', () => {
+      spyOn(loadingService, 'loadingOn');
+      component.clearFilterUsers();
+
+      expect(loadingService.loadingOn).toHaveBeenCalled();
+    });
+    it('should set pageIndex to 0', () => {
+      component.pageIndex = 5; // Set pageIndex to a non-zero value.
+      component.clearFilterUsers();
+
+      expect(component.pageIndex).toBe(0);
+    });
+
+    it('should call displayPagination method', () => {
+      spyOn(component, 'displayPagination');
+      component.clearFilterUsers();
+
+      expect(component.displayPagination).toHaveBeenCalled();
+    });
+
+    it('should call getPaginationData', () => {
+      const mockObservable = of(/* mock API response */);
+      spyOn(component, 'getPaginationData').and.returnValue(of([]));
+
+      component.clearFilterUsers();
+      expect(component.getPaginationData).toHaveBeenCalled();
+    });
+  });
+
+  describe('cancelPreviousAPICall', () => {
+    it('should call next and complete on ngUnsubscribe$', () => {
+      const nextSpy = spyOn(component['ngUnsubscribe$'], 'next');
+      const completeSpy = spyOn(component['ngUnsubscribe$'], 'complete');
+
+      component.cancelPreviousAPICall();
+
+      expect(nextSpy).toHaveBeenCalledWith('');
+      expect(completeSpy).toHaveBeenCalled();
+    });
+    it('should assign a new instance of Subject to ngUnsubscribe$', () => {
+      const previousSubject = component['ngUnsubscribe$'];
+
+      component.cancelPreviousAPICall();
+
+      expect(component['ngUnsubscribe$']).not.toBe(previousSubject);
+      expect(component['ngUnsubscribe$'] instanceof Subject).toBeTrue();
+    });
+  });
+
+  it('should set _hidePagination to true in hidePagination()', () => {
+    component.hidePagination();
+    expect(component._hidePagination).toBeTrue();
+  });
+
+  it('should set _hidePagination to false in displayPagination()', () => {
+    component.displayPagination();
+    expect(component._hidePagination).toBeFalse();
+  });
+
+  describe('clearFilterUsers', () => {
+    it('should clear the input field value and call clearFilterUsers', () => {
+      const inputElement = document.createElement('input') as HTMLInputElement;
+      inputElement.value = 'Sample Filter';
+
+      spyOn(component, 'clearFilterUsers');
+
+      component.cleanFilterInput(inputElement);
+
+      expect(inputElement.value).toBe('');
+
+      expect(component.clearFilterUsers).toHaveBeenCalled();
+    });
+  });
+
+  it('should call next and complete on ngUnsubscribe$', () => {
+    spyOn(component['ngUnsubscribe$'], 'next');
+    spyOn(component['ngUnsubscribe$'], 'complete');
+
+    component.ngOnDestroy();
+
+    expect(component['ngUnsubscribe$'].next).toHaveBeenCalledWith('');
+    expect(component['ngUnsubscribe$'].complete).toHaveBeenCalled();
   });
 });
--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,13 +1,22 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import {
   BehaviorSubject,
   Observable,
+  Subject,
+  debounceTime,
   finalize,
   map,
   switchMap,
+  takeUntil,
   tap,
 } from 'rxjs';
 import { User } from 'src/app/shared/models/user.model';
@@ -19,13 +28,15 @@ import { UsersService } from 'src/app/shared/services/users.service';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnInit, AfterViewInit {
-  users: MatTableDataSource<User> | undefined;
+export class UserListComponent implements OnInit, AfterViewInit, OnDestroy {
+  users!: MatTableDataSource<User>;
   displayedColumns: string[] = ['id', 'name', 'email'];
-  pageSize: number = 10;
-  pageIndex: number = 0;
-  totalUsers: number = 0;
+  _hidePagination = false;
+  pageSize!: number;
+  pageIndex!: number;
+  totalUsers!: number;
   isLoading$: Observable<boolean> = new BehaviorSubject(false);
+  private ngUnsubscribe$ = new Subject();
 
   @ViewChild(MatSort) sort: MatSort | undefined;
 
@@ -35,6 +46,7 @@ export class UserListComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit(): void {
+    this.initializePagination();
     this.getUsers();
   }
 
@@ -43,10 +55,23 @@ export class UserListComponent implements OnInit, AfterViewInit {
   }
 
   /**
-   * Since the API does not return the total count in the pagination response,
-   * we need to call the API twice during the initialization of the page. One API call is made to retrieve the paginated data,
-   * and another API call is made to retrieve all the user count. This is a workaround due to the limitations of the mock API.
-   * If the API provides the total count in the pagination response, we can optimize the code to make a single API call.
+   * Initializes the pagination settings (page size, current page index, total user count).
+   */
+  initializePagination() {
+    this.pageSize = 10;
+    this.pageIndex = 0;
+    this.totalUsers = 0;
+  }
+
+  /**
+ /**
+   * @description Since the API does not return the total count in the pagination response,
+   * we need to call the API twice during the initialization of the page.
+   * One API call is made to retrieve the paginated data,
+   * and another API call is made to retrieve all the user count.
+   * This is a workaround due to the limitations of the mock API.
+   * If the API provides the total count in the pagination response,
+   * we can optimize the code to make a single API call.
    */
   getUsers(): void {
     this.loadingService.loadingOn();
@@ -54,29 +79,118 @@ export class UserListComponent implements OnInit, AfterViewInit {
       .getUsers()
       .pipe(
         tap((users) => (this.totalUsers = users.length)),
-        switchMap(() => this.getPaginationData()),
-        finalize(() => this.loadingService.loadingOff())
+        switchMap(() => this.getPaginationData())
       )
-      .subscribe((users) => {
-        this.users = new MatTableDataSource(users);
-        if (this.sort) this.users.sort = this.sort;
-      });
+      .subscribe();
   }
 
-  getPaginationData() {
-    return this.usersService.getUsers(this.pageIndex, this.pageSize);
+  /**
+   * Applies sorting to the retrieved user data and updates the MatTableDataSource.
+   * @param users Array of User objects representing the users to be displayed in the table.
+   */
+  applySortingOnResponse(users: User[]) {
+    this.users = new MatTableDataSource(users);
+    if (this.sort) this.users.sort = this.sort;
   }
 
+  /**
+   * Fetches paginated user data based on the current pagination settings and filter value.
+   * @param filter Optional filter string to search for specific users by name.
+   * @returns An observable of paginated user data.
+   */
+  getPaginationData(filter?: string) {
+    return this.usersService
+      .getUsers(this.pageIndex, this.pageSize, filter)
+      .pipe(
+        tap((users) => this.applySortingOnResponse(users)),
+        finalize(() => this.loadingService.loadingOff()),
+        takeUntil(this.ngUnsubscribe$)
+      );
+  }
+
+  /**
+   * Handles page change events triggered by the Angular Material paginator.
+   * Fetches new paginated user data and updates the table when the page changes.
+   * @param event The PageEvent object containing the new page index and page size.
+   */
   onPageChange(event: PageEvent): void {
     this.pageIndex = event.pageIndex;
     this.pageSize = event.pageSize;
 
     this.loadingService.loadingOn();
-    this.getPaginationData()
-      .pipe(finalize(() => this.loadingService.loadingOff()))
-      .subscribe((users) => {
-        this.users = new MatTableDataSource(users);
-        if (this.sort) this.users.sort = this.sort;
-      });
+    this.getPaginationData().subscribe();
+  }
+
+  /**
+   * Filters the users based on the provided input filter string.
+   * Hides pagination and shows the filtered data in the table. (Since the API does not support filtering with search)
+   * @param event The input event containing the filter value.
+   */
+  filterUsers(event: any) {
+    if (!event || !event.target) return;
+
+    // Cancel the previous API call
+    this.cancelPreviousAPICall();
+
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+
+    if (filterValue) {
+      this.hidePagination();
+      this.loadingService.loadingOn();
+      this.getPaginationData(filterValue).pipe(debounceTime(300)).subscribe();
+    } else {
+      this.clearFilterUsers();
+    }
+  }
+
+  /**
+   * Clears the filter and displays paginated data in the table from page 1.
+   */
+  clearFilterUsers() {
+    this.loadingService.loadingOn();
+    this.pageIndex = 0;
+    this.displayPagination();
+    this.getPaginationData().subscribe();
+  }
+
+  /**
+   * Cancels the previous API call by unsubscribing from the current subject,
+   * and creates a new subject for upcoming subscriptions.
+   */
+  cancelPreviousAPICall(): void {
+    this.ngUnsubscribe$.next('');
+    this.ngUnsubscribe$.complete();
+    this.ngUnsubscribe$ = new Subject();
+  }
+
+  /**
+   * Hides pagination by setting the flag to true.
+   */
+  hidePagination() {
+    this._hidePagination = true;
+  }
+
+  /**
+   * Displays pagination by setting the flag to false.
+   */
+  displayPagination() {
+    this._hidePagination = false;
+  }
+
+  /**
+   * Clears the filter input by setting its value to an empty string.
+   * Then, clears the filter and shows paginated data in the table.
+   * @param inputElement The HTMLInputElement containing the filter value.
+   */
+  cleanFilterInput(inputElement: HTMLInputElement) {
+    inputElement.value = '';
+    this.clearFilterUsers();
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe$.next('');
+    this.ngUnsubscribe$.complete();
   }
 }
