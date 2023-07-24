7bc6d76ed55a825186de029b8ef53bc4c159bde8
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-07-24 07:50:17 +0100

    write unit test for auth interceptor

--- src/app/core/http/auth-interceptor.service.spec.ts
+++ src/app/core/http/auth-interceptor.service.spec.ts
@@ -1,20 +1,49 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
 import { AuthInterceptor } from './auth-interceptor.service';
 import { SocialAuthServiceConfigMock } from 'src/app/testing/social-auth.mock';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthInterceptor', () => {
-  let service: AuthInterceptor;
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
+  let authService: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [AuthInterceptor, SocialAuthServiceConfigMock],
+      providers: [
+        AuthService, // Inject the necessary dependencies.
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthInterceptor,
+          multi: true,
+        },
+        SocialAuthServiceConfigMock,
+      ],
     });
-    service = TestBed.inject(AuthInterceptor);
+
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+    authService = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should add Authorization header with bearer token', () => {
+    const mockToken = 'mock_token';
+    spyOn(authService, 'getToken').and.returnValue(mockToken);
+    httpClient.get('/api/data').subscribe();
+    const req = httpMock.expectOne('/api/data');
+    expect(req.request.headers.has('Authorization')).toBeTrue();
+    expect(req.request.headers.get('Authorization')).toBe(
+      `Bearer ${mockToken}`
+    );
   });
 });
