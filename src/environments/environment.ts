2f5aa4fc485d6a602fb15123d507f245f73cefcf
Sumit Jaiswal <jaiswalsumit18@gmail.com>
2023-07-24 07:54:55 +0100

    Update auth url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://644a9189a8370fb321528794.mockapi.io/api/v1/',
-  auth_uri: 'https://oauth2.googleapis.com/tokeninfo',
+  auth_uri: 'https://mock-api-nodejs.netlify.app/.netlify/functions/api/',
 };
